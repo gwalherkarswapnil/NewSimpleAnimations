168f9096f19281204ac236f6169b98f9222591b7
Swapnil <swapnil@Swapnils-MacBook-Air.local>
2024-08-04 23:55:24 +0530

    New

---
+++ New/NewBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewBundle.swift
+//  New
+//
+//  Created by Swapnil on 04/08/24.//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewBundle: WidgetBundle {
+    var body: some Widget {
+        New()
+        NewLiveActivity()
+    }
+}
